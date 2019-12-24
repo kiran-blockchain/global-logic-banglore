ec0a6a751d1c6f2890b4a0f5c52f49b40ac1f2b1
Kiran Paturi <kpaturi@ipromoteu.com>
2019-12-24 14:25:48 +0530

    Fixed porducts

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -8,9 +8,9 @@ export class ProductService {
   public cart:any;
   constructor() {
     this.products = [
-      { id: 1, name: "iPhone", price: 20000, quantity: 0 },
-      { id: 2, name: "samsung", price: 40000, quantity: 0 },
-      { id: 3, name: "Nokia", price: 50000, quantity: 0 }
+      { id: 1, name: "iPhone", price: 20000, quantity: 0 ,shippingDate:'12-11-2019'},
+      { id: 2, name: "samsung", price: 40000, quantity: 0,shippingDate:'12-11-2019' },
+      { id: 3, name: "Nokia", price: 50000, quantity: 0,shippingDate:'12-1-2019'}
     ];
     this.cart =[];
   }
